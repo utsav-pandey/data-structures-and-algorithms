bfd66861956bf4d5f8a9c11e4ba3954ffd6be0cf
utsav-pandey <utsavpandey107@gmail.com>
2026-09-19 23:12:39 +0530

    Added some more patterns

--- C++ Fundamentals/Lecture2.cpp
+++ C++ Fundamentals/Lecture2.cpp
@@ -586,7 +586,7 @@ int main()
               * * *
             * * * *
 */
-
+/*
 int main()
 {
     int n, i, j, space;
@@ -594,11 +594,14 @@ int main()
     cin >> n;
 
     for(i=1; i<=n; i++){
+
+        // Printing the space
         space = n-i;
         while(space > 0){
             cout << "  ";
             space -- ;
         }
+        // Printing the *
         for(j=1; j<=i; j++)
         {
             cout<< "* ";
@@ -606,4 +609,109 @@ int main()
         cout << endl;
     }
 }
+*/
 
+/*
+     19)    * * * * 
+            * * * 
+            * *
+            * 
+*/
+
+/*
+int main()
+{
+    int n, i, j;
+
+    cout<< "Enter N: ";
+    cin>> n;
+
+    for(i=1; i<=n; i++){
+        for (j=i; j<=n; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+*/
+/*
+     20)          1
+                2 3
+              4 5 6
+            7 8 9 10
+*/
+/*
+int main()
+{
+    int n, i, j;
+    cout << "Enter N: ";
+    cin >> n;
+    int value = 1;
+    for(i=0; i<n; i++){
+        // Printing the space
+        int space = n-i;
+        while(space > 0){
+            cout << "  ";
+            space --;
+        }
+
+        for(j=0; j<= i; j++){
+            cout << value << " ";
+            value ++ ;
+        }
+        cout << endl;
+
+    }
+}
+*/
+
+/*
+     21)          1
+                1 2 1
+              1 2 3 2 1
+            1 2 3 4 3 2 1
+*/
+
+int main()
+{
+    int n, i, j;
+    cout << "Enter N: ";
+    cin >> n;
+    for(i=1; i<=n; i++){
+
+        // Printing the space
+        int space = n - i;
+        while ( space > 0){
+            cout << "  ";
+            space-- ;
+        }
+
+        // Printing the first half
+        int value = 1;
+        for(j=1; j<=i; j++)
+        {
+            cout << value << " ";
+            value ++;
+        }
+
+        // Printinng the 2nd half
+        value = i;
+        for(j =2; j<=i; j++)
+        {
+            value --;
+            cout << value << " ";
+        }
+
+        cout << endl;
+
+    }
+}
+
+// ================= Practice Question ===================
+/*
+     18)    1 2 3 4 5 5 4 3 2 1
+            1 2 3 4 * * 4 3 2 1
+            1 2 3 * * * * 3 2 1
+            1 2 * * * * * * 2 1
+            1 * * * * * * * * 1
+*/
